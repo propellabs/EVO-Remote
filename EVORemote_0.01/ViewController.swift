82552ac4069665d8bbb63b5e6cd9263f6e6d02a6
Reed Miller <Reed@Reeds-iMac.local>
2015-05-25 13:23:28 -0600

    Initial Commit

---
+++ EVORemote_0.01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EVORemote_0.01
+//
+//  Created by Reed Miller on 5/25/15.
+//  Copyright (c) 2015 com.RMTEK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
